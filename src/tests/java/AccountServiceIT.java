0bd9ff9e15939c7fe5dd216ca7cef22abdfa7d42
ingthorb@gmail.com <ingthorb@gmail.com>
2016-11-02 00:08:07 +0000

    Account Service tests complete

--- src/tests/java/AccountServiceIT.java
+++ src/tests/java/AccountServiceIT.java
@@ -1,32 +1,44 @@
 import TestModels.Login;
-import data.AccountData;
+import data.AccountDataGateway;
+import exceptions.UnauthorizedException;
 import is.ruframework.data.RuData;
+import is.ruframework.data.RuDataAccessFactory;
+import is.ruframework.domain.RuException;
 import models.UserModel;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import server.App;
+
+
 /**
  * Created by Ingthor on 1.11.2016.
  */
 public class AccountServiceIT extends RuData{
+
     App app;
-    AccountData acc = new AccountData();
+    AccountDataGateway accountDataGateway;
     final String url = "http://127.0.0.1:8080/accounts/user/";
     final String loginFailed = "http://127.0.0.1:8080/accounts/login/";
+
     //User that exists in my database!
     UserModel userExisting = new UserModel("Mcfly","MartyJunior","marty@future.is","1234");
+
     //User with wrong password
-    //Created a Special Model to login
     Login wrongPassword = new Login("MartyJunior","1233333");
-    UserModel newUser = new UserModel("Marty Mcfly","111423111611211","marty@future.is","1234");
+
+    //Need to change before test
+    UserModel newUser = new UserModel("Marty Mcfly","Back","marty@future.is","1234");
+
     RestTemplate restTemplate = new RestTemplate();
     HttpHeaders headers = new HttpHeaders();
 
@@ -39,24 +51,26 @@ public class AccountServiceIT extends RuData{
 
         //Start the server.
         app.start();
-
+        //Initialize accountDataGateway
+        RuDataAccessFactory factory = null;
+        try {
+            factory = RuDataAccessFactory.getInstance("data.xml");
+        } catch (RuException e) {
+            e.printStackTrace();
+        }
+        accountDataGateway = (AccountDataGateway)factory.getDataAccess("AccountDataGateway");
         RestTemplate restTemplate = new RestTemplate();
         headers.setContentType(MediaType.APPLICATION_JSON);
     }
-    //Send in new user
     /**
      * Test to see if the user is added successfully to the database
      * Should return the Id and token
      */
-   /* @Test
+    @Test
     public void testAddUser()
     {
-        //Works sending and receiving a user
-        //Request er body,seinasta er response
         String result = restTemplate.postForObject(url,newUser,String.class);
-        System.out.println(result);
         JSONParser parser = new JSONParser();
-
         JSONObject jsonObj = new JSONObject();
         try{
             jsonObj = (JSONObject) parser.parse(result);
@@ -65,52 +79,52 @@ public class AccountServiceIT extends RuData{
         {
             pe.printStackTrace();
         }
-        Integer output =(int) (long) jsonObj.get("id");
+        int passwordReturned = (int) (long) jsonObj.get("id");
+        String Token = jsonObj.get("token").toString();
         //Get the Id from the User
-        System.out.println("BEFORE query");
-       String query = "SELECT id FROM ru.users WHERE userName = " + newUser.getUserName();
-        ResultSet rs =
-       // UserModel newestUser = acc.checkIfUserExists(newUser);
-        System.out.println(userExists.size());
-       // int ID = newestUser.getId();
-        //System.out.print(ID);
-        //assertEquals(ID ,output);
+        int idFromDb = 0;
+       try {
+           idFromDb = accountDataGateway.getUserIdFromToken(Token);
+       }
+       catch(UnauthorizedException une)
+       {
+           une.printStackTrace();
+       }
+        assertEquals(passwordReturned ,idFromDb);
     }
-*/
+
     /**
      * Test to see what happens when trying to add a user that exists
-     * Should return 412 Precondition failed
+     * Should return 409 Conflict
      */
     @Test
     public void testAddUserExists()
     {
-        //Should be Not modified
-        String result = "412 Precondition Failed";
+        //Should be Conflict
+        String result = "409 Conflict";
         try {
             result = restTemplate.postForObject(url, userExisting, String.class);
         }
         catch (HttpClientErrorException ex)
         {
-            System.out.println(ex.getMessage());
             assertEquals(result,ex.getMessage());
         }
     }
     /**
      * Test to see what happens when user tries to log in with the wrong password
-     * Should return 412 Precondition failed with the Explanation: "Incorrect password"
+     * Should return 401 Unauthorized failed with the Explanation: "Incorrect password"
      */
     @Test
     public void testLoginFailed()
     {
         //Should be Unauthorized 401
-        String explanation = "412 Precondition Failed";
+        String explanation = "401 Unauthorized";
         String result = null;
         try {
             result = restTemplate.postForObject(loginFailed, wrongPassword, String.class);
         }
         catch (HttpClientErrorException ex)
         {
-            System.out.println(ex.getMessage());
             assertEquals(explanation,ex.getMessage());
         }
     }
